ca06bf783b36debc90790a206416f22f6a73d795
LoneHusko <90392521+LoneHusko@users.noreply.github.com>
2024-09-20 09:36:27 +0200

    generate student ids (should work in theory)

--- backend/chalk_sis/student/urls.py
+++ backend/chalk_sis/student/urls.py
@@ -3,4 +3,5 @@ from student import response
 
 urlpatterns = [
     path('manage_student', response.manage_student, name='manage_student'),
+    path('generate_ids', response.generate_ids, name='generate_ids'),
 ]
