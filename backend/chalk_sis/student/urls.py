9473604dff3b7243cb9bf0d0916031b355536893
LoneHusko <90392521+LoneHusko@users.noreply.github.com>
2024-09-24 09:06:52 +0200

    add subject to students, get subject stats, get all subjects or per grade, fixed permission check wrapper

--- backend/chalk_sis/student/urls.py
+++ backend/chalk_sis/student/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('generate_ids', response.generate_ids, name='generate_ids'),
     path('all', response.all_student, name='all_student'),
     path('stat', response.statistics, name='statistics'),
+    path('<int:student_id>/add_subject/<int:subject_id>', response.add_subject, name='add_subject'),
 ]
