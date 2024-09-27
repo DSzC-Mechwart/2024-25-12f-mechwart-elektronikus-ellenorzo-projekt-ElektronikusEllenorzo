9877256e028b7490e54563f6e60501ba5b5331dc
Horváth Dávid <127218549+Horvath-David@users.noreply.github.com>
2024-09-27 08:05:46 +0200

    made UserData fields optional

--- frontend/API/models/UserData.cs
+++ frontend/API/models/UserData.cs
@@ -5,13 +5,13 @@ public record UserDataJSON {
     public int for_user { get; init; }
     public string role { get; init; } = "";
     public string name { get; init; } = "";
-    public int student_class { get; init; }
-    public string mothers_name { get; init; } = "";
-    public string place_of_birth { get; init; } = "";
-    public string date_of_birth { get; init; } = "";
-    public string address { get; init; } = "";
-    public int profession { get; init; }
+    public int? student_class { get; init; }
+    public string? mothers_name { get; init; } = "";
+    public string? place_of_birth { get; init; } = "";
+    public string? date_of_birth { get; init; } = "";
+    public string? address { get; init; } = "";
+    public int? profession { get; init; }
     public string? dorm_name { get; init; }
     public bool is_first_login { get; init; }
-    public int[] subjects { get; init; } = [];
+    public int[]? subjects { get; init; } = [];
 }
