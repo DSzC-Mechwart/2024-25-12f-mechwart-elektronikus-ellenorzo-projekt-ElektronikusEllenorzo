956371465c167f8faeaa97b4bfb0900fe064df8c
Barna István <155573677+IstvanBarna815@users.noreply.github.com>
2024-10-18 20:38:15 +0200

    Diák Választó Felület cs

---
+++ VS Solution/IKT_II_Derecske_Holding_EE/Ablakok/TanuloPanel/DiakValasztoFelulet/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace LoginInterface
+{
+    public partial class DiakValasztoFelulet : Window, INotifyPropertyChanged
+    {
+        private string _userName = "Szabó Balázs";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string UserName
+        {
+            get => _userName;
+            set
+            {
+                if (_userName != value)
+                {
+                    _userName = value;
+                    OnPropertyChanged(nameof(UserName));
+                }
+            }
+        }
+
+        public DiakValasztoFelulet()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
